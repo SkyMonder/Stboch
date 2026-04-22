805cd1f53575cb9e9ea09fe23654efe0f44778c5
SkyMonder <rilloperdagelo@gmail.com>
2026-04-22 22:10:05 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,10 +6,8 @@ mkdir -p temp
 cd temp
 wget -q https://github.com/official-stockfish/Stockfish/releases/download/sf_18/stockfish-ubuntu-x86-64-bmi2.tar
 tar -xf stockfish-ubuntu-x86-64-bmi2.tar
-cp stockfish/stockfish-ubuntu-x86-64-bmi2 ../engine
+cp stockfish/stockfish-ubuntu-x86-64-bmi2 ../stockfish
 cd ..
 rm -rf temp
-chmod +x ./engine
-
-echo "=== Запускаем движок Stockfish ==="
+chmod +x ./stockfish
 exec gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:$PORT engine:app
